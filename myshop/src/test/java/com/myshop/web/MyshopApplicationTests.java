4653ba1372a70d4403cbc760893aab6003f43bf3
jinseok87 <a871120@gamil.com>
2022-12-01 02:24:41 +0900

    pro05

---
+++ myshop/src/test/java/com/myshop/web/MyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myshop.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
