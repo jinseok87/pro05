da0a5aa4c7d4b66e8825bbee771b4363f83fe42b
jinseok87 <a871120@gamil.com>
2022-12-07 05:26:41 +0900

    pro05

--- myshop/src/main/java/com/myshop/web/controller/Homcontroller.java
+++ myshop/src/main/java/com/myshop/web/controller/Homcontroller.java
@@ -1,8 +1,20 @@
 package com.myshop.web.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class Homcontroller {
-
+	@RequestMapping("/index")
+	public String index() {
+		return "home.index";
+	}
+	@RequestMapping("/help")
+	public String aaa() {
+		
+		return "home.help";
+	}
+	
+	
 }
